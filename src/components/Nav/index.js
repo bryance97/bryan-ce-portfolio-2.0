4edcdc49a45a08c46fd72b9b3cd9a48edf91b6b0
bryance97 <bryancifuentesescobar@gmail.com>
2022-02-03 10:36:59 -0500

    adding header, footer

---
+++ src/components/Nav/index.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function NavBar(props) {
+    const tabs = ['About Me', 'Contact', 'Portfolio', 'Resume'];    
+    return (
+        <div>
+            <ul id="nav-bar" className="nav navbar">
+                {tabs.map(currentTab => (
+                    <li key={currentTab} >
+                        <a
+                            href={'#' + currentTab.toLowerCase()}
+                            onClick={() => props.handlePageChange(currentTab)}
+                            className={
+
+                                props.currentPage === currentTab ? `nav-link active text-success` : 'nav-link'
+                            }
+                        >
+                            {currentTab}
+                        </a>
+                    </li>
+                ))
+                }
+            </ul >
+        </div>
+    );
+}
+
+export default NavBar;
